bf5f83bb47194f50840b7646a8cc5445d8775281
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-08-12 13:19:16 +0900

    LinkedList complete

--- LinkedList/LinkedList/LinkedList.h
+++ LinkedList/LinkedList/LinkedList.h
@@ -1,10 +1,69 @@
 #pragma once
 
+#include <assert.h>
 
-
-
-struct Node {
-	int data;
+typedef int ListElementType;
+class List {
+public:
+	List();
+	void insert(const ListElementType& elem);
+	bool first(ListElementType& elem);
+	bool next(ListElementType& elem);
+private:
+	struct Node;
 	typedef Node* Link;
-	Link next;
+	struct Node {
+		int data;
+		Link next;
+	};
+	Link head;
+	Link tail;
+	Link current;
 };
+
+List::List() {
+	head = 0;
+	tail = 0;
+	current = 0;
+}
+
+void List::insert(const ListElementType& elem) {
+	Link addedNode = new Node;
+	assert(addedNode);
+	addedNode->data = elem;
+	if (head == 0) {
+		head = addedNode;
+	}
+	else {
+		tail->next = addedNode;
+	}
+	tail = addedNode;
+	addedNode->next = 0;
+}
+bool List::first(ListElementType& elem) {
+	
+	if (head == 0) {
+		return false;
+	}
+	else {
+		elem = head->data;
+		current = head;
+		return true;
+	}
+}
+
+bool List::next(ListElementType& elem) {
+
+	if (current->next == 0) {
+		return false;
+	}
+	else {
+		current = current->next;
+		elem = current->data;
+		return true;
+	}
+}
+
+
+
+
