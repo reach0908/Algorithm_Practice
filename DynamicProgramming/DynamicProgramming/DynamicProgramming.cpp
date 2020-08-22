6e4e5caa1da8ed9e2dd1a40ae082b81ed4581814
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-08-22 19:37:08 +0900

    add Dynamic Programming

---
+++ DynamicProgramming/DynamicProgramming/DynamicProgramming.cpp
@@ -0,0 +1,34 @@
+﻿// DynamicProgramming.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+// 재귀용법을 활용한 피보나치 수열
+#define SIZE 1000
+
+long long save[SIZE] = {0,1,};
+
+int fibo(int num) {
+	if (num<=1)
+	{
+		return num;
+	}
+	else { return fibo(num - 1) + fibo(num - 2); }
+}
+
+double dynamicFibo(int num) {
+	if (save[num] > 0) {
+		return save[num];
+	}
+	return 
+}
+
+//동적계획법을 이용한 피보나치 수열
+
+int main()
+{
+	cout << fibo(4) << endl;
+    
+}
+
