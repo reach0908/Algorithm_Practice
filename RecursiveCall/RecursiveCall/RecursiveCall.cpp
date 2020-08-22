9bf6abe1f1b09a7201c26486d6dc5780b0cceaa9
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-08-22 14:40:05 +0900

    add example

--- RecursiveCall/RecursiveCall/RecursiveCall.cpp
+++ RecursiveCall/RecursiveCall/RecursiveCall.cpp
@@ -1,8 +1,9 @@
 ﻿
 
 #include <iostream>
+#include <random>
 
-
+#define ARR_SIZE 10
 
 using namespace std;
 
@@ -16,10 +17,44 @@ int factoiral(int num) {
 	}
 }
 
+int multiple(int num) {
+	if (num <= 1) {
+		return num;
+	}
+	else
+	{
+		return num * multiple(num-1);
+	}
+}
+int sum(int arr[],int arrSize) {
+	if (arrSize <= 1) {
+		return arr[0];
+	}
+	else
+	{
+		return arr[arrSize - 1] + sum(arr, arrSize - 1);
+	}
+}
+
 int main()
 {
 
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> dis(1, 100);
+
 	cout << factoiral(4) << endl;
+	cout << multiple(4) << endl;
+	
+	int arr[ARR_SIZE];
+	for (int i = 0; i < ARR_SIZE; i++)
+	{
+		arr[i] = dis(gen);
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	cout << sum(arr, ARR_SIZE);
+
     return 0;
 }
 
@@ -43,3 +78,8 @@ int func2(int num) {
 	int return_Value = num*func2(num - 1); - 입력보다 작은 값 
 	return return_Value; - 결과값
 }*/
+
+//재귀호출은 스택의 전형적인 예이다.
+// - 함수는 내부적으로 스택처럼 관리된다.
+
+
