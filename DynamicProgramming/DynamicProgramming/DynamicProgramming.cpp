908a7b0e2dd85f7955e2d1334878fb160bed50b4
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-08-22 19:50:23 +0900

    fibo complete

--- DynamicProgramming/DynamicProgramming/DynamicProgramming.cpp
+++ DynamicProgramming/DynamicProgramming/DynamicProgramming.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // 재귀용법을 활용한 피보나치 수열
 #define SIZE 1000
 
-long long save[SIZE] = {0,1,};
+double save[SIZE] = {0,};
 
 int fibo(int num) {
 	if (num<=1)
@@ -18,17 +18,23 @@ int fibo(int num) {
 }
 
 double dynamicFibo(int num) {
-	if (save[num] > 0) {
-		return save[num];
+	save[0] = 0;
+	save[1] = 1;
+	for (int i = 2; i <= num+1; i++)
+	{
+		save[i] = save[i - 1] + save[i - 2];
 	}
-	return 
+	return save[num];
 }
 
 //동적계획법을 이용한 피보나치 수열
 
 int main()
 {
-	cout << fibo(4) << endl;
+	cout << fibo(10) << endl;
+	cout << dynamicFibo(10) << endl;
+
+	//동적계획법으로 만든 피보나치수열이 재귀로 만든 피보나치보다 훨씬 시간이 적게 걸린다. 
     
 }
 
