60ccbb60d55ff146409690839a6658283d70e3b0
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-08-23 17:39:35 +0900

    add Sequential Search

---
+++ SequentialSearch/SequentialSearch/SequentialSearch.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+#define ARR_SIZE 10
+
+bool sequentialSearch(int arr[], int search,int size) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i]==search)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dis(1, 100);
+
+    int arr[ARR_SIZE];
+    for (int i = 0; i < ARR_SIZE; i++)
+    {
+        arr[i] = dis(gen);
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout << "찾으려는 값을 입력하세요 :";
+    int search;
+    cin >> search;
+    if (sequentialSearch(arr, search, ARR_SIZE)) {
+        cout << "찾았습니다.";
+    }
+    else
+    {
+        cout << "찾지못했습니다.";
+    }
+    
+    return 0;
+}
+
